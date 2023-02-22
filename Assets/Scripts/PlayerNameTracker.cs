ce3c332b59944e6c38970d2cc902d76adb180e52
Matt <trotrex91@hotmail.com>
2023-02-22 23:56:03 +0100

    Add tracking of players names

---
+++ Assets/Scripts/PlayerNameTracker.cs
@@ -0,0 +1,99 @@
+using System;
+using FishNet.Connection;
+using FishNet.Object;
+using FishNet.Object.Synchronizing;
+using FishNet.Transporting;
+
+// <summary>
+// Let's player set their name and sync it iwth others.
+// </summary>
+public class PlayerNameTracker : NetworkBehaviour
+{
+
+    // <summary>
+    // Called when any player changes their name.
+    // </summary>
+    public static event Action<NetworkConnection, string> OnNameChange;
+
+    // <summary>
+    // Collection of each player name for connections.
+    // </summary>
+    [SyncObject]
+    private readonly SyncDictionary<NetworkConnection, string> _playerNames = new SyncDictionary<NetworkConnection, string>();
+
+    // <summary>
+    // Singelton instacne of this object.
+    // </summary>
+    private static PlayerNameTracker _instance;
+
+    private void Awake()
+    {
+        _instance = this;
+        _playerNames.OnChange += _playerNames_OnChange;
+    }
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        base.NetworkManager.ServerManager.OnRemoteConnectionState += ServerManager_OnRemoteConnectionState;
+    }
+    public override void OnStopServer()
+    {
+        base.OnStopServer();
+        base.NetworkManager.ServerManager.OnRemoteConnectionState -= ServerManager_OnRemoteConnectionState;
+    }
+
+    // <summary>
+    // Let's player set their name and sync it iwth others
+    // </summary>
+    private void ServerManager_OnRemoteConnectionState(NetworkConnection arg1, RemoteConnectionStateArgs arg2)
+    {
+        if (arg2.ConnectionState != RemoteConnectionState.Started)
+        {
+            _playerNames.Remove(arg1);
+        }
+    }
+
+    // <summary>
+    // Let's player set their name and sync it iwth others
+    // </summary>
+    private void _playerNames_OnChange(SyncDictionaryOperation op, NetworkConnection key, string value, bool asServer)
+    {
+        if (op == SyncDictionaryOperation.Add || op == SyncDictionaryOperation.Set)
+        {
+            OnNameChange.Invoke(key, value);
+        }
+    }
+
+    // <summary>
+    // Let's player set their name and sync it iwth others
+    // </summary>
+    public static string GetPlayerName(NetworkConnection conn)
+    {
+        if (_instance._playerNames.TryGetValue(conn, out string result))
+            return result;
+        else
+            return string.Empty;
+    }
+
+    // <summary>
+    // Let's player set their name and sync it iwth others
+    // </summary>
+    [Client]
+    public static void SetName(string name)
+    {
+        _instance.ServerSetName(name);
+    }
+
+    // <summary>
+    // Let's player set their name and sync it iwth others
+    // </summary>
+    // <param name="name"></param>
+    // <param name="sender"></param>
+    [ServerRpc(RequireOwnership = false)]
+    private void ServerSetName(string name, NetworkConnection sender = null)
+    {
+        _playerNames[sender] = name;
+    }
+}
+
